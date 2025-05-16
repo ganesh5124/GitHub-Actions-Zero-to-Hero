d71ec1392612278288778b101ec6598096254b09
Ganesh5124 <pvgkumar2001@gmail.com>
2025-05-16 13:10:29 +0530

    updated

--- src/addition.py
+++ src/addition.py
@@ -3,6 +3,14 @@
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+
+def test_sub():
+    assert sub(1, 4) == -3
+    assert sub(5, 2) == 1
